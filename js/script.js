d7eaf01b9881bf35ac57a1613ae0e9446ee4f960
Cristian R <crincon012@gmail.com>
2020-11-04 20:50:43 -0800

    day two commit

--- js/script.js
+++ js/script.js
@@ -6,3 +6,12 @@ var food = 'Carne Asada';
 
 console.log(name); // My name
 console.log(food) // My favorite food
+
+
+answer = 'Supercalifragilisticexpialidocious'
+console.log(answer.length)
+console.log(typeof 34)
+
+const BigInt = 438472349587498573498n;
+console.log(BigInt)
+console.log(typeof 438472349587498573498n)

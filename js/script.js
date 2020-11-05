cf519d411af8945bcef6a907f9dbad802d20413d
Cristian R <crincon012@gmail.com>
2020-11-04 16:32:17 -0800

    take home assignemnt

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* This is the assignment I was given for the
+first javascript for begginers class */
+
+var name = 'Cristian';
+var food = 'Carne Asada';
+
+console.log(name); // My name
+console.log(food) // My favorite food
